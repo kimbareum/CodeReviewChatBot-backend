a8a324572c1f8556a76dc00c47f22561e460800f
kimbareum <right.kim93@gmail.com>
2023-07-31 17:00:41 +0900

    배포 주소 CORS origin whitelist에 추가

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['52.78.109.184', 'codereviewchatbot.space']
+ALLOWED_HOSTS = ['codereviewchatbot.space']
 
 
 # Application definition
@@ -140,7 +140,7 @@ SIMPLE_JWT = {
 }
 
 # CORS
-CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000','http://localhost:3000']
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000','http://localhost:3000', 'https://kimbareum.github.io/']
 CORS_ALLOW_CREDENTIALS = True
 
 # CSRF
